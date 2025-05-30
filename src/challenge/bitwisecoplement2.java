a396b4cea57ab71efa29379b5f5e1eee4cf93d8a
thunderavi <hittuprasad0909@gmail.com>
2025-05-30 13:55:56 +0530

    Added new file

--- src/challenge/bitwisecoplement2.java
+++ src/challenge/bitwisecoplement2.java
@@ -2,15 +2,15 @@ package challenge;
 
 import java.util.Scanner;
 
-public class bitwisecoplement {
+public class bitwisecoplement2 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        System.out.println("Welcome to Not/Complimant  operator");
+        System.out.println("Welcome to Showcase LeftShift Operator  operator");
         System.out.print("Enter the number :");
         int num =read.nextInt();
 
 
-        int result = ~num;
+        int result = num << 1;
         System.out.println("THe result is :" +result);
 
     }

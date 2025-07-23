7b0e38c5a1a24e6605dab3e139d33239d24d4b90
thunderavi <hittuprasad0909@gmail.com>
2025-07-23 14:14:00 +0530

    Throw and Throws

--- src/ExceptionHandling_File_H/calculator.java
+++ src/ExceptionHandling_File_H/calculator.java
@@ -4,29 +4,45 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        System.out.println("Welcome to division calculator");
-        System.out.print("Please enter your first number : ");
+        a();
+    }
+    private static void a(){
+        b();
+    }
+    private static void b(){
+        c();
+    }
+    private static void c(){
+        d();
+    }
+    private static void d(){
+            Scanner read = new Scanner(System.in);
+            System.out.println("Welcome to division calculator");
+            System.out.print("Please enter your first number : ");
 
-        int first = read.nextInt();
-        System.out.print("Please enter your Second number : ");
+            int first = read.nextInt();
+            System.out.print("Please enter your Second number : ");
 
-        int second = read.nextInt();
+            int second = read.nextInt();
 
-        try {
-            int[] a = new int[5];
-//            System.out.printf("Result is %d", a[6]);
-            a[6] = first / second;
-            int result = first / second;
-//            System.out.printf("Result is %d", a[6]);
-            System.out.printf("Result is %d", result);
-        }catch (ArithmeticException exception){
-            System.out.printf("%s , enter valid values",
-                    exception.getMessage());
+            try {
+                int[] a = new int[5];
+           System.out.printf("Result is %d", a[6]);
+                a[6] = first / second;
+          System.out.printf("Result is %d", a[6]);
+                int result = first / second;
+                System.out.printf("Result is %d", result);
+            }catch (ArithmeticException exception){
+                System.out.printf("%s , enter valid values",
+                        exception.getMessage());
 //        }catch (ArrayIndexOutOfBoundsException exc){
 //            System.out.println("Array is out of Bound");
-        }catch (Throwable th){  // more general and parent
-            System.out.println("General Exception");
+            }catch (Throwable th){  // more general and parent
+                System.out.println("General Exception");
+                throw th;
+            }
         }
     }
-}
+
+
+

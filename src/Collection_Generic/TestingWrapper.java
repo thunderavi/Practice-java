16f99c373c18e13a212dd5856a5b3d8b79d010ff
thunderavi <hittuprasad0909@gmail.com>
2025-08-14 19:13:12 +0530

    wrapper class conclude

---
+++ src/Collection_Generic/TestingWrapper.java
@@ -0,0 +1,11 @@
+package Collection_Generic;
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+        // wapper object
+        Integer first = Integer.valueOf(55);
+        Integer Second = 55;
+
+        first.byteValue();
+    }
+}
---
+++ src/Collection_Generic/Wrapper_Classes.java
@@ -0,0 +1,17 @@
+package Collection_Generic;
+
+public class Wrapper_Classes {
+
+    public static void main(String[] args) {
+//        Integer first = Integer.valueOf(55);
+        Integer first = 55;
+
+        //AutoBoxing
+        System.out.println(first);
+        int Second = first;
+
+        //unboxing
+
+
+    }
+}
